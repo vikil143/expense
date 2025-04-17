08469682329757cae412196e4432fcec6d36da03
vikil143 <vikil.lakkavatri25@gmail.com>
2025-04-17 21:44:46 +0530

    loader done

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+import { commonStyles } from '@myapp/utilities/common-styles';
+import { SCREEN_WIDTH } from '@myapp/utilities/common-data';
 
 export default function Loader() {
+  const width = SCREEN_WIDTH - 20 - 20
   return (
-    <View>
-        <View style={{ padding: 30, backgroundColor: "#ddd" }} />
-        <View style={{ padding: 10 }} />
-        <View style={{ padding: 250, backgroundColor: "#ddd" }} />
+    <View style={[commonStyles.pA20]}>
+        <SkeletonPlaceholder>
+            <SkeletonPlaceholder.Item width={width} height={60} />
+            <SkeletonPlaceholder.Item marginTop={6} width={width} height={250} />
+            <SkeletonPlaceholder.Item marginTop={6} width={width} height={60} />
+            <SkeletonPlaceholder.Item marginTop={6} width={width} height={150} />
+        </SkeletonPlaceholder>
     </View>
   )
 }
